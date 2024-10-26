44a4a163d47968403bdde69f3010422ef08e1de0
ARUNNG2004 <arun1990govind@gmail.com>
2024-10-26 12:40:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+import Todo from './Todo.jsx'
+function App() {
+
+
+  return (
+    <div className="App bg-dark ">
+      <Todo/>
+    </div>
+
+  )
+}
+
+export default App
